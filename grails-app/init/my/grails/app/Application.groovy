ad4b9cfaa64d8dc8d1a9c8a710e605c842e3b226
Jemerson Maia <jemerson.maia@asaas.com.br>
2022-09-06 09:21:32 -0300

    Monitoring

--- grails-app/init/my/grails/app/Application.groovy
+++ grails-app/init/my/grails/app/Application.groovy
@@ -2,7 +2,9 @@ package my.grails.app
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.metrics.export.prometheus.EnablePrometheusMetrics
 
+@EnablePrometheusMetrics
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
